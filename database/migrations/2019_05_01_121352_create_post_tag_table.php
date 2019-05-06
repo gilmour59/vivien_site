1fcfeca23e548a1305b7527c0580113a7128d978
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-06 09:29:36 +0800

    undo ondelete('cascade')

--- database/migrations/2019_05_01_121352_create_post_tag_table.php
+++ database/migrations/2019_05_01_121352_create_post_tag_table.php
@@ -18,8 +18,8 @@ class CreatePostTagTable extends Migration
             $table->unsignedMediumInteger('post_id');
             $table->unsignedMediumInteger('tag_id');
 
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('tag_id')->references('id')->on('tags');
         });
     }
 
