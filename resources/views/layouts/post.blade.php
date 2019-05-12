93473cdac5e2bfefbe2518d80c8818c8d4298959
gilmour59 <gilmouralmalbisdev@gmail.com>
2019-05-13 02:20:50 +0800

    fonts and layout/post changes

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -56,6 +56,7 @@
                         <li><a href="index.html">Home</a></li>
                         <li class="menu-has-children"><a href="">Packages</a>
                             <ul>
+                                <li><a href="blog-home.html">All Packages</a></li>
                                 @foreach ($categories as $category)
                                     <li><a href="blog-home.html">{{ $category->type }}</a></li>
                                 @endforeach
@@ -80,7 +81,7 @@
             <div class="container">
 
                 <div class="row">
-                    <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="col-lg-4  col-md-6 col-sm-6">
                         <div class="single-footer-widget">
                             <h6>About Agency</h6>
                             <p>
@@ -88,34 +89,30 @@
                             </p>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="single-footer-widget">
                             <h6>Navigation Links</h6>
                             <div class="row">
                                 <div class="col">
                                     <ul>
                                         <li><a href="#">Home</a></li>
-                                        <li><a href="#">Feature</a></li>
-                                        <li><a href="#">Services</a></li>
-                                        <li><a href="#">Portfolio</a></li>
+                                        <li><a href="#">Packages</a></li>
                                     </ul>
                                 </div>
                                 <div class="col">
                                     <ul>
-                                        <li><a href="#">Team</a></li>
-                                        <li><a href="#">Pricing</a></li>
-                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">About</a></li>
                                         <li><a href="#">Contact</a></li>
                                     </ul>
                                 </div>										
                             </div>							
                         </div>
                     </div>							
-                    <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="col-lg-4  col-md-6 col-sm-6">
                         <div class="single-footer-widget">
                             <h6>Newsletter</h6>
                             <p>
-                                For business professionals caught between high OEM price and mediocre print and graphic output.									
+                                Receive Updates through Emails.									
                             </p>								
                             <div id="mc_embed_signup">
                                 <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
@@ -127,21 +124,6 @@
                                 </form>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget mail-chimp">
-                            <h6 class="mb-20">InstaFeed</h6>
-                            <ul class="instafeed d-flex flex-wrap">
-                                <li><img src="img/i1.jpg" alt=""></li>
-                                <li><img src="img/i2.jpg" alt=""></li>
-                                <li><img src="img/i3.jpg" alt=""></li>
-                                <li><img src="img/i4.jpg" alt=""></li>
-                                <li><img src="img/i5.jpg" alt=""></li>
-                                <li><img src="img/i6.jpg" alt=""></li>
-                                <li><img src="img/i7.jpg" alt=""></li>
-                                <li><img src="img/i8.jpg" alt=""></li>
-                            </ul>
-                        </div>
                     </div>						
                 </div>
 
@@ -152,8 +134,6 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                     <div class="col-lg-4 col-sm-12 footer-social">
                         <a href="#"><i class="fa fa-facebook"></i></a>
                         <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-                        <a href="#"><i class="fa fa-behance"></i></a>
                     </div>
                 </div>
             </div>
