155d752e7046e8784e5c9e9971029f39d6862741
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-14 14:16:56 +0800

    category model fillable type => name

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['type'];
+    protected $fillable = ['name'];
 
     public function posts(){
         return $this->hasMany('App\Post');
