908e990dc6ee0a4c227c3a6ae9265ffb83900f87
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-14 11:40:30 +0800

    added initial show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,80 +1,27 @@
 @extends('layouts.post')
 
-@section('title')
-    {{ $post->title }}
-@endsection
-
-@section('header')
-<!-- Header -->
-    <header class="header text-white h-fullscreen pb-80" style="background-image: url({{ asset('storage/' . $post->image) }});" data-overlay="5">
-        <canvas class="constellation" data-radius="0"></canvas>
-        <div class="container text-center">
-            <div class="row h-100">
-                <div class="col-lg-8 mx-auto align-self-center">
-                    <p class="opacity-70 text-uppercase small ls-1">{{ $post->category->name }}</p>
-                    <h1 class="display-4 mt-7 mb-8">{{ $post->title }}</h1>
-                    <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">Test Name</a></p>
-                    <p><img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="..."></p>
-                </div>
-                <div class="col-12 align-self-end text-center">
-                    <a class="scroll-down-1 scroll-down-white" href="#section-content"><span></span></a>
-                </div>
+@section('banner')
+    <section class="relative about-banner">	
+        <div class="overlay overlay-bg"></div>
+        <div class="container">				
+            <div class="row d-flex align-items-center justify-content-center">
+                <div class="about-content col-lg-12">
+                    <h1 class="text-white">
+                        {{ $post->title }}				
+                    </h1>	
+                    <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span><a href="blog-home.html">{{ $post->category->type }} Packages </a> <span class="lnr lnr-arrow-right"></span> <a href="blog-single.html"> {{ $post->title }}</a></p>
+                </div>	
             </div>
         </div>
-    </header><!-- /.header -->
+    </section>
 @endsection
 
 @section('content')
-    <main class="main-content">
-        <!--
-        |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
-        | Blog content
-        |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
-        !-->
-        <div class="section" id="section-content">
-            <div class="container">
-                {!! $post->content !!}
-                <div class="row">
-                    <div class="gap-xy-2 mt-6">
-                        @foreach ($post->tags as $tag)
-                            <a class="badge badge-pill badge-secondary" href="#">{{ $tag->name }}</a>
-                        @endforeach                                   
-                    </div>
-                </div>              
+    <section class="post-content-area single-post-area">
+        <div class="container">
+            <div class="single-post">                
+                {{ $post->content }}
             </div>
-        </div>
-        <!--
-        |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
-        | Comments
-        |‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
-        !-->
-        <div class="section bg-gray">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <hr>
-                        <div id="disqus_thread"></div>
-                        <script>
-                        /**
-                        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-                        
-                            var disqus_config = function () {
-                                this.page.url = '{{ config('app.url') }}posts/{{ $post->id }}';  // Replace PAGE_URL with your page's canonical URL variable
-                                this.page.identifier = '{{ $post->id }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                            };
-                            
-                            (function() { // DON'T EDIT BELOW THIS LINE
-                                var d = document, s = d.createElement('script');
-                                s.src = 'https://world-escapes-travel-and-tours.disqus.com/embed.js';
-                                s.setAttribute('data-timestamp', +new Date());
-                                (d.head || d.body).appendChild(s);
-                            })();
-                        </script>
-                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
+        </div>	
+    </section>
 @endsection
