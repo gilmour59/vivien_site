30e8380da727e0657b72b4302be6e7a65c81bc33
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-16 15:53:26 +0800

    show page fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,15 +3,7 @@
 @section('banner')
     <section class="relative about-banner">	
         <div class="overlay overlay-bg"></div>
-        <div class="container">
-            <div class="search row mb-5">
-                <div class="col-7 col-sm-5 col-md-4 col-lg-3 col-xl-3 offset-5 offset-sm-7 offset-md-8 offset-lg-9 offset-xl-9">
-                    <form class="search-form" action="#">
-                        <input placeholder="Search" name="search" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search'" >
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-            </div>				
+        <div class="container">				
             <div class="row d-flex align-items-center justify-content-center">
                 <div class="about-content col-lg-12">
                     <h1 class="text-white">
@@ -27,8 +19,18 @@
 @section('content')
     <section class="post-content-area single-post-area">
         <div class="container">
-            <div class="single-post">                
-                {{ $post->content }}
+            <div class="search row mb-5">
+                <div class="col-7 col-sm-5 col-md-4 col-lg-3 col-xl-3 offset-5 offset-sm-7 offset-md-8 offset-lg-9 offset-xl-9">
+                    <form class="search-form" action="#">
+                        <input placeholder="Search" name="search" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Search'" >
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="single-post">                
+                    {{ $post->content }}
+                </div>
             </div>
         </div>	
     </section>
