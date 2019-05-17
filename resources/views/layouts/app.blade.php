9cc111e1c36d7736fae2656e10965708dc446c15
gilmour59 <gilmouralmalbisdev@gmail.com>
2019-05-18 03:10:54 +0800

    removed profile link in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,10 +59,6 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('users.profile') }}">
-                                        My Profile
-                                    </a>
-
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
