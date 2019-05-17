ed8d408208963b213e15343ddcf515b3a60c5e97
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-17 13:30:06 +0800

    added messenger

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -30,7 +30,37 @@
         <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">				
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     </head>
-    <body>	
+    <body>
+
+        <!-- Messenger -->
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                xfbml            : true,
+                version          : 'v3.3'
+                });
+            };
+
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+
+        <!-- Your customer chat code -->
+        <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1400216213452617"
+        theme_color="#0084ff"
+        logged_in_greeting="Hi! You can Inquire here :D"
+        logged_out_greeting="Hi! You can Inquire here :D">
+        </div>
+
         <header id="header">
             <div class="header-top">
                 <div class="container">
