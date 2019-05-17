f4222af77f01ca3a378606add27ac014f05190c1
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-05-17 11:34:45 +0800

    added disqus

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -34,4 +34,32 @@
             </div>
         </div>	
     </section>
+
+    <section class="comments-area" style="margin-top: 0px; background: white;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <div id="disqus_thread"></div>
+                    <script>
+                    /**
+                    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                    
+                        var disqus_config = function () {
+                            this.page.url = '{{ config('app.url') }}posts/{{ $post->id }}';  // Replace PAGE_URL with your page's canonical URL variable
+                            this.page.identifier = '{{ $post->id }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                        };
+                        
+                        (function() { // DON'T EDIT BELOW THIS LINE
+                            var d = document, s = d.createElement('script');
+                            s.src = 'https://world-escapes-travel-and-tours.disqus.com/embed.js';
+                            s.setAttribute('data-timestamp', +new Date());
+                            (d.head || d.body).appendChild(s);
+                        })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                </div>
+            </div>            
+        </div>	                                             				
+    </section>
 @endsection
